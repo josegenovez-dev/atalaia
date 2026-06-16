76b4f7f0f99e6cdd552f7a18fde751df29b0ed71
josegenovez-dev <jose.genovez@shopee.com>
2026-06-16 15:48:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "🛡️ Atalaia Online"
+
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
@@ -14,12 +15,12 @@ def webhook():
 
     if data.get("event_type") == "event_verification":
         challenge = data.get("event", {}).get("seatalk_challenge")
-        if challenge:
-            return jsonify({
-                "seatalk_challenge": challenge
-            }), 200
 
-    return jsonify({"status": "ok"}), 200
+        if challenge:
+            return challenge, 200
+
+    return "ok", 200
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
