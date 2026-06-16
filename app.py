e59d771c6893fb221ef6f910dac7211da453e293
josegenovez-dev <jose.genovez@shopee.com>
2026-06-16 14:01:08 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "🛡️ Atalaia Online"
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    data = request.json
+    print(data)
+    return jsonify({"status": "ok"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
