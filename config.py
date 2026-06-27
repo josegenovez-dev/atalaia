60cc7adc22f0dba5481804a43d7e763fa6ee80ac
josegenovez-dev <jose.genovez@shopee.com>
2026-06-27 11:29:39 -0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+APP_ID = os.getenv("APP_ID")
+APP_SECRET = os.getenv("APP_SECRET")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+GOOGLE_SERVICE_ACCOUNT_JSON = os.getenv("GOOGLE_SERVICE_ACCOUNT_JSON")
+
+BASE_URL = "https://openapi.seatalk.io"
+
+PLANILHAS = {
+    "farol": {
+        "id": "1LhUnFClJo_KfKFKHSzNBOawJ3KOT0s-CKd8pRAyR2o",
+        "abas": ["Histórico de Entregas"]
+    }
+}
