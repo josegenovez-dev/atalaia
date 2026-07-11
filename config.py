81e987f60c45506602a4637c50536728fd41d532
josegenovez-dev <jose.genovez@shopee.com>
2026-07-11 14:45:27 -0300

    Update config.py

--- config.py
+++ config.py
@@ -3,13 +3,8 @@ import os
 APP_ID = os.getenv("APP_ID")
 APP_SECRET = os.getenv("APP_SECRET")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-GOOGLE_SERVICE_ACCOUNT_JSON = os.getenv("GOOGLE_SERVICE_ACCOUNT_JSON")
 
-BASE_URL = "https://openapi.seatalk.io"
-
-PLANILHAS = {
-    "farol": {
-        "id": "1LhUnFfCldo_KtKFKHSzNBOawJ3KOT0s-CKd8pRAyR2o",
-        "abas": ["Farol"]
-    }
-}
+BASE_URL = os.getenv(
+    "SEATALK_BASE_URL",
+    "https://openapi.seatalk.io",
+)
